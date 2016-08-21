33994990f893e946f304722a985c3a116114abdc
Tyler Benster <tbenst@gmail.com>
2016-08-21 14:33:52 -0700

    refactoring unit_spike_trains

--- glia/eyecandy.py
+++ glia/eyecandy.py
@@ -91,7 +91,7 @@ def get_stimulus_start_times(analog_file, threshold=10000):
 def get_stimulus_from_eyecandy(start_times, eyecandy_gen):
     """Return list of tuples (start time, corresponding eyecandy_gen)"""
     # compensate for weird analog behavior at end of recording
-    start_times.pop()
+    # start_times.pop()
     return list((map(lambda x: (x, next(eyecandy_gen)['value']), start_times)))
     
 def create_experiments(unit: np.ndarray, stimulus_list,
