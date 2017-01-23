b09dce806d3e9fa1cc8db8506faf83675918f250
Tyler Benster <tbenst@gmail.com>
2017-01-23 09:29:48 -0800

    docker fix

--- scripts/acuity.py
+++ scripts/acuity.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from functools import update_wrapper, partial
-from tests.conftest import display_top, tracemalloc
+# from tests.conftest import display_top, tracemalloc
 import logging
 logger = logging.getLogger('glia')
 
