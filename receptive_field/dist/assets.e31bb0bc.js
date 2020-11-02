a159094f21e4c869b777891760a41671302033ad
tbenst <lensman@tylerbenster.com>
2020-11-01 19:50:05 -0800

    add images

--- receptive_field/dist/assets.e31bb0bc.js
+++ receptive_field/dist/assets.e31bb0bc.js
@@ -26663,7 +26663,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45119" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38887" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
